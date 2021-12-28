3e06db379e0f95b36aec51c6745c1e6b71255dbb
Hoang <quochoang32b@gmail.com>
2021-12-28 13:39:42 +0700

    Debug

--- doan_1/settings.py
+++ doan_1/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y31un+pwgt9ixfs@y!xn-7y(q1#cb20u5(_f*pj1p%%sncnal9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['doan1-quochoang.herokuapp.com','127.0.0.1']
 
